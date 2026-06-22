07c69ab640099aa703a1c4146f03c1ea898c5a18
funnysmile <vadimyakob920@gmail.com>
2026-06-22 16:43:56 +0000

    remove axios in clienttable

--- src/components/ClientTable.js
+++ src/components/ClientTable.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import ClientDetailsModal from "./ClientDetailsModal";
-import axios from "axios";
 
 export default function ClientTable({ clients }) {
   const [selected, setSelected] = useState(null);
